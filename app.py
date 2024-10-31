4f00f7ea81ea1977ab8f6af0a2db9608835927aa
Bezalel <bs0583251661@gmail.com>
2024-10-31 13:47:01 +0200

    querys 1-2

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = Flask
 
-#with app.app_context():
- #   init_db()
+with app.app_context():
+    init_db()
 
 app.add_url_rule('/graphql',
                  view_func=GraphQLView.as_view('graphql',
@@ -25,5 +25,4 @@ def shutdown_server(exception=None):
     db_session.remove()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0',
-            port=5001)
+    app.run(host="0.0.0.0", port=5000)
--- types_1/query.py
+++ types_1/query.py
@@ -1,30 +1,31 @@
 import graphene
 import graphene as g
+from graphene import DateTime
 
 from db.database import db_session
-from db.models import Missions
+from db.models import Missions as MissionModel
 from types_1.mission_type import MissionType
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
-class User(SQLAlchemyObjectType):
+class Missions(SQLAlchemyObjectType):
     class Meta:
-        model = Missions
+        model = MissionModel
         interfaces = (graphene.relay.Node, )
 
 class Query(g.ObjectType):
-    mission_by_id = g.Field(Missions, mission_id=g.Int(required=True))
-    # missions_by_dates_range = g.List(Missions, start_date=g.String, end_date=g.String)
+    mission_by_id = g.Field(Missions, _id=g.Int(required=True))
+    missions_by_dates_range = g.List(Missions,start_date=g.Date(required=True), end_date=g.Date(required=True))
     # missions_by_countries = g.List(Missions, countries = g.String(required=True))
     # attack_results = g.List(Missions, target_type_name=g.String(required=True))
 
-    # @staticmethod
-    # def resolve_mission_by_id(mission_id):
-    #     return db_session.query(Missions).filter(MissionType.mission_id == mission_id).first()
-    #
-    # @staticmethod
-    # def resolve_missions_by_dates_range(start_date, end_date):
-    #     pass
-    #
+    @staticmethod
+    def resolve_mission_by_id(root, info, _id):
+         return db_session.query(MissionModel).get(_id)
+
+    @staticmethod
+    def resolve_missions_by_dates_range(root, info,  start_date, end_date):
+         return db_session.query(MissionModel).filter(MissionModel.mission_date.between(start_date, end_date)).all()
+    # #
     # @staticmethod
     # def resolve_missions_by_countries(countries):
     #     pass
